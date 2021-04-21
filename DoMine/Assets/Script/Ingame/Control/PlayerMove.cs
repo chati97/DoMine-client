6a894e1a3a40bd2fceda184b0fafd0d820bc593b
minho9710 <minho9710@naver.com>
2021-04-22 03:21:18 +0900

    4/22 작업(기초이동, 벽만드는알고리즘작업)
    기초이동과 카메라 구현
    
    벽만드는 알고리즘개발중
    타일맵 일정크기로만듬
    맨끝은 파괴불가한 벽으로 막히도록설정
    프리팹생성은 미구현

---
+++ DoMine/Assets/Script/Ingame/Control/PlayerMove.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace DoMine
+{
+    public class PlayerMove : MonoBehaviour
+    {
+        public float moveSpeed;
+        // Start is called before the first frame update
+        void Start()
+        {
+            
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            float _inputX = Input.GetAxisRaw("Horizontal");
+            float _inputY = Input.GetAxisRaw("Vertical");
+            transform.Translate(new Vector2(_inputX,_inputY)*Time.deltaTime*moveSpeed);
+        }
+    }
+}
+
---
+++ DoMine/Assets/Script/Ingame/Map/MapController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DoMine
+{
+    public class MapController : MonoBehaviour
+    {
+        int[,] mapArray = new int[100,100];
+        [SerializeField] GameObject breakable;
+        [SerializeField] GameObject unreakable;
+
+        public int[,] MakeMapArr()
+        {
+            int[,] mapArray = new int[100, 100];
+            for(int i = 0; i<100; i++)
+            {
+                mapArray[0, i] = 2;
+                mapArray[99, i] = 2;
+                mapArray[i, 0] = 2;
+                mapArray[i, 99] = 2;
+            }
+            for(int i = 1; i<100; i++)
+            {
+                for(int j = 1; j < 100; j++)
+                {
+                    mapArray[i, j] = 1; //나머지는 부서지는벽으로설정
+                }
+            }
+            mapArray[49, 49] = 0;
+            return mapArray;
+        }
+
+        public void AddMap(int[,] mapArray)
+        {
+            foreach(int type in mapArray)
+            {
+                switch (type)
+                {
+                    case 0:
+                        break;
+                    case 1:
+                        //부서지는 벽 생성
+                        break;
+                    case 2:
+                        //안부서지는 벽 생성
+                        break;
+
+                }   
+            }
+        }
+        
+    }
+}
+
