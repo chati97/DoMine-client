25270b8065d4b43553eb3f42eea9f2084b4e37d4
chati97 <67995114+chati97@users.noreply.github.com>
2021-05-09 18:18:35 +0900

    server(~ing)

--- DoMine/Assets/Script/server/Launcher.cs
+++ DoMine/Assets/Script/server/Launcher.cs
@@ -20,7 +20,7 @@ namespace Com.MyCompany.MyGame
         /// </summary>
         string gameVersion = "1";
 
-
+        bool isConnectiong;
         #endregion
 
 
@@ -66,6 +66,7 @@ namespace Com.MyCompany.MyGame
         /// </summary>
         public void Connect()
         {
+            isConnectiong = true;
             progressLabel.SetActive(true);
             controlPanel.SetActive(false);
             // we check if we are connected or not, we join if we are , else we initiate the connection to the server.
@@ -91,6 +92,10 @@ namespace Com.MyCompany.MyGame
         public override void OnConnectedToMaster()
         {
             Debug.Log("PUN Basics Tutorial/Launcher: OnConnectedToMaster() was called by PUN");
+            if(isConnectiong)
+            {
+                PhotonNetwork.JoinRandomRoom();
+            }
         }
 
 
@@ -101,7 +106,20 @@ namespace Com.MyCompany.MyGame
             Debug.LogWarningFormat("PUN Basics Tutorial/Launcher: OnDisconnected() was called by PUN with reason {0}", cause);
         }
 
-
+        public override void OnJoinRandomFailed(short returnCode, string message)
+        {
+            Debug.Log("PUN Basics Tutorial/Launcher:OnJoinRandomFailed() was called by PUN. No random room available, so we create one.\nCalling: PhotonNetwork.CreateRoom");
+            PhotonNetwork.CreateRoom(null, new RoomOptions());
+        }
+        public override void OnJoinedRoom()
+        {
+            Debug.Log("PUN Basics Tutorial/Launcher: OnJoinedRoom() called by PUN. Now this client is in a room.");
+            if(PhotonNetwork.CurrentRoom.PlayerCount == 1)
+            {
+                Debug.Log("We load the 'room for 1 '");
+                PhotonNetwork.LoadLevel("Room for 1");
+            }
+        }
         #endregion
 
     }
