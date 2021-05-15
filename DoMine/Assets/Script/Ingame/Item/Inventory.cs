dee32dc283e226ecc61eca34e58bf83bacd5ae9c
minho9710 <minho9710@naver.com>
2021-05-15 18:39:33 +0900

    아이템추가해보려는 시도 1

---
+++ DoMine/Assets/Script/Ingame/Item/Inventory.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace DoMine
+{
+    [Serializable]
+    public class Inventory
+    {
+        bool Gold;
+    }
+}
