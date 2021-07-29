dd206923a490b51d8e476583bfc0643d5789e953
chati97 <67995114+chati97@users.noreply.github.com>
2021-07-29 22:27:58 +0900

    player

--- DoMine/Assets/Script/server/ServerManager.cs
+++ DoMine/Assets/Script/server/ServerManager.cs
@@ -1,23 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using Photon.Bolt;
 
     public class ServerManager : GlobalEventListener
     {
         public static ServerManager NM;
         private void Awake() => NM = this;
-
-
         public List<BoltEntity> players = new List<BoltEntity>();
         public BoltEntity myPlayer;
 
         public GameObject SpawnPrefab;
 
-        public override void SceneLoadLocalDone(string scene, IProtocolToken token)
-        {
-            Debug.Log("ABC");
-            var spawnPos = new Vector2(Random.Range(50, 52), 50);
-            myPlayer = BoltNetwork.Instantiate(BoltPrefabs.Player, spawnPos, Quaternion.identity);
-        }
+    public override void SceneLoadLocalDone(string scene, IProtocolToken token)
+    {
+        var spawnPos = new Vector3(Random.Range(50, 52), 50, 0);
+        myPlayer = BoltNetwork.Instantiate(BoltPrefabs.Player, spawnPos, Quaternion.identity);
+        myPlayer.TakeControl();
     }
+}
