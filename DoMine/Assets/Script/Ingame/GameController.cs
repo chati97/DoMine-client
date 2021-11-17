c2bc4bfafc262c201a863d3d66054ea2079206b0
minho9710 <minho9710@naver.com>
2021-11-17 23:20:01 +0900

    폰트크기조절, 입금메시지오류수정

--- DoMine/Assets/Script/Ingame/GameController.cs
+++ DoMine/Assets/Script/Ingame/GameController.cs
@@ -171,7 +171,7 @@ namespace DoMine
         {
             playerList[evnt.Player] = 2;
             goldSaved++;
-            MessageCreate((playerNameList[evnt.Player] + "가 <color=yellow>코인</color>을 입금했습니다.").ToString());
+            UC.MessagePrint((playerNameList[evnt.Player] + "가 <color=yellow>코인</color>을 입금했습니다.").ToString());
         }//금 입금 콜백
         public override void OnEvent(WallDestoryed evnt)
         {
